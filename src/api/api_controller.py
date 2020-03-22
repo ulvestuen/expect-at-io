21f02139004a66293fc8865a5f06d154942b8eee
Idar Ulvestad Haugstuen <idaruh@gmail.com>
2020-03-22 21:29:23 +0100

    Rewrite of application in order to use the connexion library to define the API from OpenApi Spec.

--- src/api/api_controller.py
+++ src/api/api_controller.py
@@ -1,53 +1,57 @@
 import json
 from flask import request, jsonify
+from service.test_service import TestService
+
+test_service = TestService()
 
 
-class ApiController:
-    test_service = None
+def load():
+    collection_file = request.files["collection"]
+    io_data_file = request.files["testdata"]
 
-    def __init__(self, service):
-        self.test_service = service
+    test_collection = json.load(collection_file)
+    test_data = json.load(io_data_file)
 
-    def load(self):
-        collection_file = request.files["collection"]
-        io_data_file = request.files["testdata"]
+    pre_request_event_index = [e["listen"] for e in test_collection["event"]].index("prerequest")
+    pre_request_script = test_collection["event"][pre_request_event_index]["script"]["exec"]
+    with open("resources/postman_pre-request.json", "r") as f:
+        pre_request_script += json.load(f)
 
-        collection = json.load(collection_file)
-        test_data = json.load(io_data_file)
+    test_service.init()
+    test_service.collection = test_collection
+    test_service.input_data = test_data["input"]
+    test_service.output_data = test_data["output"]
 
-        pre_request_event_index = [e["listen"] for e in collection["event"]].index("prerequest")
-        pre_request_script = collection["event"][pre_request_event_index]["script"]["exec"]
-        with open("resources/postman_pre-request.json", "r") as f:
-            pre_request_script += json.load(f)
+    return "Test case loaded. Input data available at /inputdata \n" \
+           "Expected output data available at /outputdata \n" \
+           "Postman collection available at /collection"
 
-        self.test_service.init()
-        self.test_service.collection = collection
-        self.test_service.input_data = test_data["input"]
-        self.test_service.output_data = test_data["output"]
 
-        return "Test case loaded. Input data available at /inputdata \n" \
-               "Expected output data available at /outputdata \n" \
-               "Postman collection available at /collection"
+def run():
+    return test_service.run_test()
 
-    def run(self):
-        return self.test_service.run_test()
 
-    def results(self):
-        return jsonify(self.test_service.test_results)
+def results():
+    return jsonify(test_service.test_results)
 
-    def collection(self):
-        return jsonify(self.test_service.collection)
 
-    def input_data(self):
-        return jsonify(self.test_service.input_data)
+def collection():
+    return jsonify(test_service.collection)
 
-    def output_data(self):
-        return jsonify(self.test_service.output_data)
 
-    def ready(self):
-        return jsonify({"ready": self.test_service.ready_for_new_test})
+def input_data():
+    return jsonify(test_service.input_data)
 
-    def busy(self):
-        self.test_service.ready_for_new_test = False
-        self.test_service.run_check_for_result()
-        return jsonify({"ready": self.test_service.ready_for_new_test})
+
+def output_data():
+    return jsonify(test_service.output_data)
+
+
+def ready():
+    return jsonify({"ready": test_service.ready_for_new_test})
+
+
+def busy():
+    test_service.ready_for_new_test = False
+    test_service.run_check_for_result()
+    return jsonify({"ready": test_service.ready_for_new_test})
--- src/application.py
+++ src/application.py
@@ -1,22 +1,10 @@
-from flask import Flask
+import connexion
 from flask_cors import CORS
-from api.api_controller import ApiController
-from service.test_service import TestService
 import sys
 
-app = Flask(__name__)
-CORS(app)
-test_service = TestService()
-api = ApiController(test_service)
-app.add_url_rule('/load', 'load', api.load, methods=["POST"])
-app.add_url_rule('/run', 'run', api.run, methods=["POST"])
-app.add_url_rule('/results', 'results', api.results, methods=["GET"])
-app.add_url_rule('/collection', 'collection', api.collection, methods=["GET"])
-app.add_url_rule('/inputdata', 'input_data', api.input_data, methods=["GET"])
-app.add_url_rule('/outputdata', 'output_data', api.output_data, methods=["GET"])
-app.add_url_rule('/ready', 'ready', api.ready, methods=["GET"])
-app.add_url_rule('/busy', 'busy', api.busy, methods=["GET"])
-
+app = connexion.App(__name__, specification_dir='openapi/')
+app.add_api('openapi.yml')
+CORS(app.app)
 
 if __name__ == '__main__':
     host_ip = sys.argv[1] if len(sys.argv) > 1 else '127.0.0.1'
